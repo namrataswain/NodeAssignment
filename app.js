bbc496025f6cb8649141bac6216ce4cb304b50d5
namrataswain456@gmail.com <namrataswain456@gmail.com>
2021-03-04 00:36:04 +0530

    creating and serving html pages in response

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(7000, () => {
   console.log("server started on port 7000");
