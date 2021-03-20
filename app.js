8380ab039a33b8288715048d72fa8beb378af88d
namrataswain456@gmail.com <namrataswain456@gmail.com>
2021-03-20 22:37:58 +0530

    assignment 4

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const sequelize = require("./util/databse");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const app = express();
+const csrfProtection = csrf();
+
+const MONGODB_URI =
+  "mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop";
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -19,31 +33,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(aboutRoutes);
 
-sequelize
-  .sync()
-  .then((result) => {
-    //console.log(result);
-    app.listen(7000);
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
   })
-  .catch((err) => {
-    console.log(err);
-  });
+);
+
+// sequelize
+//   .sync()
+//   .then((result) => {
+//     //console.log(result);
+//     app.listen(7000);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 mongoose
-  .connect("")
+  .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Sushmita",
-          email: "Sush@test.com",
-          Sector: {
-            companies: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
